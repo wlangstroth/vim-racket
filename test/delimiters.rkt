3de8610a214329197e7c43a9fdb58cf1f976d473
Will Langstroth <will@langstroth.com>
2013-01-29 20:36:59 -0500

    fix delimiter test

--- test/delimiters.rkt
+++ test/delimiters.rkt
@@ -1,4 +1,5 @@
 #lang racket
-(define good-highlight '(a b #\tab))
-(define bad-highlight '(a b #[))
-(define stuff-here-still-considered-quoted '())
+(define good-highlight '(a b #tab))
+(define bad-highlight1 '(a b #[))
+(define bad-highlight2 '(a b #\tab))
+(define stuff-here-should-not-be-quoted '())
